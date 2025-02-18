589877f1d85e9de243b00190ebf1b5210a337883
Adaobi Chuma-Okeke <chumzine@gmail.com>
2025-02-19 00:09:20 +0100

    completed lab UI for hospitals

---
+++ src/components/lab/cards/labTestCard/index.tsx
@@ -0,0 +1,31 @@
+import { Button, Info, CartIconTwo, Star } from "@hexify/atoms";
+import style from "./style.module.css";
+
+export const LabTestCard = () => {
+    return (
+        <div>
+            <div className={style.card}>
+                <div className={style.testInfo}>
+                    <h6 className={style.testName}>Kidney Function Test</h6>
+                    <span className={style.description}>Measures kidney performance and detects potential kidney diseases</span>
+                    <button className={style.testInstruction}>
+                        <Info />
+                        <span>Test Instructions</span>
+                    </button>
+                </div>
+                <div className={style.actions}>
+                    <span className={style.price}>NGN 5,000.00</span>
+                    <div className={style.btnContainer}>
+                        <Button className={style.saveBtn} size="medium" variant="outlined" color="secondary" rounded>
+                            <Star fill="" stroke="currentColor" />
+                            <span>Save for later </span>
+                        </Button>
+                        <Button size="medium" variant="contained" rounded>
+                            <CartIconTwo /> <span>Add to cart</span>
+                        </Button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
