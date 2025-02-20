735f4d32fee0b8baab5ff56a90c66de0f946e663
Emmanuel Daniel <3631446+emmsdan@users.noreply.github.com>
2025-02-20 04:55:29 +0100

    completed lab UI for hospitals (#7)
    Co-authored-by: Adaobi Chuma-Okeke <chumzine@gmail.com>

--- src/app/(main)/hospital/[slug]/labs/page.tsx
+++ src/app/(main)/hospital/[slug]/labs/page.tsx
@@ -1,12 +1,74 @@
+"use client";
+import React, { useState } from "react";
+import SignupBanner from "@/app/(main)/_components/signupBanner";
+import { LabTestCard } from "@/components/lab/cards/labTestCard";
+import LabDetails from "@/components/lab/labDetails";
+import { Pagination } from "@hexify/components";
+import { Chip } from "@hexify/atoms";
+import Link from "next/link";
 import { Breadcrumb } from "@hexify/atoms";
+import { ImageBackgroundBanner } from "@hexify/components";
+import { style } from "motion/react-client";
+import styles from "./page.module.css";
+
 const Lab = () => {
+  const testFilter = ["All", "Microbiology", "Heamatology", "Pathology"];
+
+  // const [activeChip, setActiveChip] = useState<number | null>(null);
+
+  const [currentPage, setCurrentPage] = React.useState(1);
+  const totalPages = 68; 
+
   return (
     <div>
       <div className="inner-wrapper">
 
-      <Breadcrumb excludePaths={["hospital"]} />
+        <Breadcrumb excludePaths={["hospital"]} />
+        <div>
+          <ImageBackgroundBanner
+            style={{
+              background: `url('https://images.pexels.com/photos/6129879/pexels-photo-6129879.jpeg?auto=compress&cs=tinysrgb&w=600')`,
+            }}
+          >
+            <LabDetails />
+          </ImageBackgroundBanner>
+        </div>
       </div>
-      this is the lab page
+      <div className={styles.container}>
+        <div className={styles.heading}>
+          <h6>Our Lab Services</h6>
+          <div>Search bar</div>
+        </div>
+        <div className={styles.filterHeading}>
+          <ul className={styles.filterList}>
+            {testFilter?.map((filter) => {
+              return <Chip designSize="large" key={filter} label={filter} />;
+            })}
+          </ul>
+          <div>filter button</div>
+        </div>
+        <div className={styles["tests-wrapper"]}>
+          <ul className={styles.testList}>
+            {Array.from({ length: 5 })?.map((_, index) => {
+              return (
+                <li key={index} className={styles.testListItem}>
+                  <Link href="?test=id">
+                    <LabTestCard />
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </div>
+      <div className={styles.pagination}>
+        <Pagination
+          currentPage={currentPage} 
+          totalPages={totalPages} 
+          onPageChange={setCurrentPage} 
+        />
+      </div>
+        <SignupBanner />
     </div>
   );
 };
---
+++ src/components/lab/labDetails/index.tsx
@@ -0,0 +1,22 @@
+import style from "./style.module.css";
+import { LocationPin } from "@hexify/atoms";
+
+const LabDetails = () => {
+    return (
+      <div className={style.bannerContainer}>
+        <div className={style.bannerHeading}>
+          Salem Specialist Hospital Laboratory
+        </div>
+        <span className={style.bannerLocation}>
+          <span>
+            <LocationPin />{" "}
+          </span>
+          <span>
+            Plot 12, Adeola Odeku Street, Victoria Island, Lagos (1.5 km from you)
+          </span>
+        </span>
+      </div>
+    );
+  };
+
+  export default LabDetails;
