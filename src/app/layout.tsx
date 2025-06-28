258219d3d36ed444518c76a6114d7680553eb14a
emmsdan <emmsdan.inc@gmail.com>
2025-06-28 21:12:17 +0100

    move branding to brand-engine

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import "@hexify/atoms/src/theme/initialize.server"
 import type { Metadata } from "next"
 import QueryProvider from "@/context/reactQueryProvider"
 import "../styles/global.css"
@@ -6,34 +7,23 @@ import SecureStorageProvider from "@/context/storage"
 import ToastProvider from "@/context/toastProvider"
 import UserLocationProvider from "@/context/location"
 import CartProvider from "@/context/cart"
-import {
-  createBranding,
-  loadStaticFile,
-} from "@hexify/atoms/src/theme/getBranding"
 import { CustomTheme } from "@hexify/atoms"
-import MaintenancePage from "@hexify/providers/src/components/MaintainanceMode"
-import textData from "../../test-data.json"
 
 export const metadata: Metadata = {
   title: "Care Konnect",
   description: "All in one health care system",
 }
 
-export default async function RootLayout({
+export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode
 }>) {
-  const data = await createBranding()
-
   return (
     <html lang="en">
       <body style={{ backgroundColor: "#F8FAF0" }}>
-        {JSON.stringify(loadStaticFile())}
-        <CustomTheme branding={data?.e || !data ? undefined : data}>
-          {data?.features?.appDisabled ? (
-            <MaintenancePage />
-          ) : (
+        <CustomTheme>
+          {
             <UserLocationProvider>
               <SecureStorageProvider>
                 <QueryProvider>
@@ -46,7 +36,7 @@ export default async function RootLayout({
                 </QueryProvider>
               </SecureStorageProvider>
             </UserLocationProvider>
-          )}
+          }
         </CustomTheme>
       </body>
     </html>
